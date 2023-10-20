be666f0da7113922d4c541ae1fef1c0474b0f3ee
SALMA55ASHRAF <116911551+SALMA55ASHRAF@users.noreply.github.com>
2023-10-20 19:02:23 +0300

    Add files via upload

---
+++ implemnt stack using modules.py
@@ -0,0 +1,13 @@
+# implemnt stack using modules 
+import collections 
+stack=collections.deque() #class from it 
+print(stack)
+# deque class have append and pop methodes to add and remove last elment from stack
+stack.append(10)
+stack.append(20)
+stack.append(30)
+print(stack)
+stack.pop()
+print(stack)
+
+
